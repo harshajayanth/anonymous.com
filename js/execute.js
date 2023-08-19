1357d3074d5e2919c511dc6f4ddb11df6cedfbd3
harsha jayanth <harshajayanth437@gmail.com>
2023-08-19 21:19:28 +0530

    second commit

--- js/execute.js
+++ js/execute.js
@@ -1,18 +1,19 @@
-function execute(command) {
 
+function execute(command) {
     if(command.includes('play')){
         const song = command.replace('play', '');
         talk('Playing ' + song);
         url="https://www.youtube.com/results?search_query="+song;
         window.open(url)
     }
-    else if(command.includes('youtube')){
-        url="https://www.youtube.com";
-        talk('Opening ' + command);
-        window.open(url)
-    }
     else if(command.includes('who are you')){
         talk('I am Anonymous. Your Voice assistant');
+    }
+    else if(command.includes('my name'||'what is my name')){
+        const username=$("#username").text();
+        const name=username.replace("Hey,","").trim();
+        talk(name)
+        $("#output").html(name)
     }
     else if(command.includes('time')){
         const time=new Date();
@@ -29,9 +30,10 @@ function execute(command) {
             $("#output").text(currenttime)
         }
     }
-    else if(command.includes("spotify")){
-        url="https://spotify.com"
-        talk('Opening spotify')
+    else if(command.includes("spotify"||"youtube"||"google")){
+        const urlinput = command.replace('open', '').trim();
+        url="https://"+urlinput+".com";
+        talk('Opening'+urlinput)
         window.open(url)
     }
     else if(command.includes("my blog")){
@@ -39,8 +41,41 @@ function execute(command) {
         talk('Opening Your Blog')
         window.open(url)
     }
-    else if(command.includes("logout")){
+    else if(command.includes("logout"||"log out")){
         talk('Logging Out!Bye!')
         location.reload()
+    }
+    else if(command.includes("open")){
+        const urlinput=command.replace("open","").trim();
+        url="https://"+urlinput+"com"
+        talk("Opening"+urlinput)
+        window.open(url)
+    }
+    else if(command.includes("what"||"who"||"how"))
+    {
+        const wikipediaApiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=${encodeURIComponent(command)}`;
+
+        $.ajax({
+            url: wikipediaApiUrl,
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                const searchResults = data.query.search;
+                let resultHtml = "";
+
+                searchResults.forEach(function (result) {
+                    resultHtml +=result.title+result.snippet;
+                });
+                $("#output").html(resultHtml);
+                talk(resultHtml);
+            },
+            error: function (error) {
+                talk("Error fetching Wikipedia content:");
+            }
+        })
+    }
+    else{
+        $("#output").text("Speak something!")
+        talk("Sorry!I am unavailable to this service");
     }
 }
--- js/index.js
+++ js/index.js
@@ -31,12 +31,12 @@ $(document).ready(function (){
 function talk(text){
     const utterance = new SpeechSynthesisUtterance(); //Text to speech Object
     utterance.text = text;
-
     // Optional: Set properties like voice, rate, pitch, etc.
-    utterance.voice = speechSynthesis.getVoices()[0]; // Get the first available voice
+    utterance.voice = speechSynthesis.getVoices()[1]; // Get the first available voice
     utterance.rate = 1;  // Speed of speech
     utterance.pitch = 1; // Pitch of speech
     speechSynthesis.speak(utterance);
+
 }
 // Define the logins object outside the event handlers
 const logins = {
@@ -46,20 +46,38 @@ const logins = {
 
 $("#create").click(function () {
     let newname = $("#newname").val();
-    let newpass = $("#newpass").val();
-
-    // Add the new login to the logins object
-    logins[newpass] = [newname];
-
-    $("#passcode").val("");
-    $("#main, #signup").hide();
-    $("#login").fadeIn();
+    let newdigit1 = $("#newdigit1").val();
+    let newdigit2 = $("#newdigit2").val();
+    let newdigit3 = $("#newdigit3").val();
+    let newdigit4 = $("#newdigit4").val();
+    let newpass=newdigit1+newdigit2+newdigit3+newdigit4;
+    if(newname!==""&&newpass!=="")
+    {
+        if(newpass in logins){
+             talk("PassCode is already taken!Enter another Passcode")
+        }
+        else{
+            talk("Profile Created successfully!")
+            logins[newpass] = [newname];
+            $("#passcode").val("");
+            $("#main, #signup").hide();
+            $("#login").fadeIn();
+        }
+    }
+    else
+    {
+        talk("Please Fill the fields")
+    }
 });
 
-$("#passcode").keypress(function (event) {
-    $(this).removeClass("animate__animated animate__headShake");
-    if (event.which === 13) {
-        let pass = $("#passcode").val();
+$("#li").click(function () {
+    $("#digit1,#digit2,#digit3,#digit4").removeClass("border-danger animate__animated animate__headShake");
+    let digit1 = $("#digit1").val();
+    let digit2 = $("#digit2").val();
+    let digit3 = $("#digit3").val();
+    let digit4 = $("#digit4").val();
+    if (digit1!==""&&digit2!==""&&digit3!==""&&digit4!=="") {
+        let pass = digit1+digit2+digit3+digit4
         if (pass in logins) {
             const name = logins[pass][0];
             $("#main").fadeIn();
@@ -70,13 +88,16 @@ $("#passcode").keypress(function (event) {
             $("#username").text("Hey, " + name);
             return name;
         } else {
-            $("#passcode").addClass("border-danger animate__animated animate__headShake");
+            $("#digit1,#digit2,#digit3,#digit4").addClass("border-danger animate__animated animate__headShake");
             talk("User Not found");
         }
     }
+    else{
+        $("#digit1,#digit2,#digit3,#digit4").addClass("border-danger animate__animated animate__headShake");
+        talk("Please Enter Passcode")
+    }
 });
-
-$("#logout").click(function (){
+$("#logout,#reload").click(function (){
     $("#passcode").val("")
     $("#main,#signup").hide();
     $("#login").fadeIn();
@@ -98,3 +119,21 @@ function checkScreenResolution() {
         }
     }
 }
+const digitInputs = document.querySelectorAll('.digit-input');
+
+digitInputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        if (input.value.length === 1) {
+            if (index < digitInputs.length - 1) {
+                digitInputs[index + 1].focus();
+            }
+        }
+        else{
+            digitInputs[index - 1].focus();
+        }
+    });
+});
+
+
+
+
